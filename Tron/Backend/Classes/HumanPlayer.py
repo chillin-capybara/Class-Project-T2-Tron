3694de24519363499a6a73343a7cf9af6c51f7b5
artem.k <45640075+artemkrenytskyi@users.noreply.github.com>
2019-06-18 20:49:12 +0200

    HumanPlayer

--- Tron/Backend/Classes/HumanPlayer.py
+++ Tron/Backend/Classes/HumanPlayer.py
@@ -3,4 +3,73 @@ from Player import Player
 # todo: Implement the Human player according to the UML
 
 class HumanPlayer(Player):
+    
+    """
+    implementation of Human Player Class
+    """
+
+    __Name = "" # Name of the player
+    
+    def getName(self) -> str:
+        """
+        get the Name from the Sever
+
+        Returns:
+            str: Name of the player
+        """
+        return self.__Name
+    
+    def setName(self, name: str):
+        """
+        TODO: DOKU
+        ...
+        ...
+        Raises:
+            TypeError
+        """
+        if type(name) == str:
+            self.__Name = name
+        else:
+            raise TypeError
+    
+    def getColor(self):
+    	"""
+		get the Color from the Sever
+        
+        Args: 
+        player ID
+        
+        Returns:
+        int
+        
+        """
+        raise NotImplementedError
+    
+    def getPosition(self):
+        """
+		get the Position from the Sever
+        
+        Args: 
+        player ID
+        
+        Returns:
+        Vect2D
+        
+        """
+        raise NotImplementedError
+    
+    def getTrack (self):
+        """
+		get the Track from the Sever
+        
+        Args: 
+        player ID
+        
+        Returns:
+        Vect2D
+        
+        """
+        raise NotImplementedError
+        
+
 	pass
