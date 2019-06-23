06578d6a1a6605c564117f9b0c1bf21d1de7fffe
Ludwig-Graef <51089466+Ludwig-Graef@users.noreply.github.com>
2019-06-23 22:53:56 +0200

    UI Stuff

--- Tron/UI/Widgets/TrackWidget.py
+++ Tron/UI/Widgets/TrackWidget.py
@@ -14,31 +14,30 @@ from Backend.Core.Vect2D import Vect2D
 
 
 class TrackWidget(Widget):
+    # function for creating the single rectangles, creating the typical snake feeling
     updatesPerSecond = 10
     fieldSize = (100, 100)
 
     def startTrack(self):
+        # function responsible for the drawing part
         self.canvas.clear()
 
         with self.canvas.before:
             Line(width=1, rectangle=(self.x + 1, self.y + 1, self.width - 2, self.height - 2))
 
+        # runs the update function every periode
         Clock.schedule_interval(self.update, 0.5 / self.updatesPerSecond)
 
 
     def update(self, *args):
+        # function for updating the linepoints and draw new
         linepoints = []
         
         # for i in range(random.randint(1, 50)):
         #     linepoints.append(Vect2D(random.randint(1, 99), random.randint(1, 99)))
         
-        for i in range(50):
-            linepoints.append(Vect2D(1+i, 10+i))
-        for t in range(len(linepoints)-3):
-            print (len(linepoints))
-            if linepoints[2*t] != linepoints[2*t+2] and linepoints[2*t+1] != linepoints[2*t+3]:
-                break
         
+        straightlinechecker(linepoints)
 
 
         self.canvas.clear()
@@ -47,6 +46,16 @@ class TrackWidget(Widget):
                 x = (point.x - 1) * 5
                 y = (point.y - 1) * 5
                 Rectangle(pos=(x, y), size=(5, 5))
+
+    def straightlinechecker(self, linepoints2):
+        # function for checking if two points in list are straight line 
+        for i in range(50):
+            linepoints2.append(Vect2D(1+i, 10+i))
+        for t in range(len(linepoints2)-3):
+            print (len(linepoints2))
+            if linepoints2[2*t] != linepoints2[2*t+2] and linepoints2[2*t+1] != linepoints2[2*t+3]:
+                break
+
 
 
     # Events
--- mainUI.py
+++ mainUI.py
@@ -17,12 +17,13 @@ from Tron.UI.Widgets.PlayerWidget import PlayerWidget
 
 # setting display size to 500, 500
 Config.set('graphics', 'resizable', '1')
-
 Config.set('graphics', 'width', '500')
 Config.set('graphics', 'height', '500')
 
+# Not using kv files for logic reasons, maybe temporary
 Builder.load_string("""
 <GameUI>: 
+    # Creates Button, where you are able to start the countdown
     Button:
         text: "Start"
         pos: 250, 250
@@ -32,6 +33,7 @@ Builder.load_string("""
             root.countdown_is_running = True
             countdown.start()
 
+    # initializes the countdown feature
     CountdownWidget:
         id: countdown
         opacity: 1 if root.countdown_is_running else 0
@@ -42,11 +44,12 @@ Builder.load_string("""
             root.do_finished()         
             trackWidget.startTrack()
 
+    # kv file for the track representation
     TrackWidget:
         id: trackWidget
         size: root.size
         opacity: 1 if root.game_is_running else 0
-
+    # kv file for displaying all ingame players with colors
     PlayerWidget:
         id: playerWidget0
         size: root.getplayerWidgetSize()[0], root.getplayerWidgetSize()[1]
@@ -62,25 +65,29 @@ class GameUI(Widget):
     playerWidgetSize = ListProperty([]) 
     labelHeight = NumericProperty()
     def getplayerWidgetSize(self):
+        # creates the hight for the widget in duty of displaying all players online
         self.playerWidgetSize = [500 , 200]
         # self.labelHeight = 3.0
         # len(Game.getPlayers())
         # self.playerWidgetSize = [int(labelHeight*100) , 100]
         return self.playerWidgetSize
+
     def do_finished(self):
+        # event handler, sets the Booleans for opacity
         def callback(_):
             # Countdown abgelaufen
             # Spiel starten ...
             self.countdown_is_running = False
             self.game_is_running = True
 
-
+        # after specified time callback function is called anbd game starts
         Clock.schedule_once(callback, 0.5)
 
 
 
 # Entry Point
 class GameApp(App):
+    # creates the Application
     def build(self):
         return GameUI()
 
