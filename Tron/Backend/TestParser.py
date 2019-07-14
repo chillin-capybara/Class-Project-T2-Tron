e2e60fa32baca96c07058899eeda3dfee9d143bf
Marcell Pigniczki <marceip97@gmail.com>
2019-07-14 22:11:11 +0200

    Parser Test extension

--- Tron/Backend/TestParser.py
+++ Tron/Backend/TestParser.py
@@ -53,9 +53,40 @@ class TestParser(unittest.TestCase):
 		msg = "pretfsdf fsdfdsf fdsfsdf fsdfsf pretty 3232 00"
 		self.assertEqual(parser(msg, player_name), 0)
 
+		# Test wins
+		self.assertEqual(parser("Marcell wins", "marcell"), 1)
+		self.assertEqual(parser("You won", "marcell"), 1)
+		self.assertEqual(parser("marcell is the best", "marcell"), 1)
+		self.assertEqual(parser("marcell is the champion", "marcell"), 1)
+		self.assertEqual(parser("marcell is the winner", "marcell"), 1)
+		self.assertEqual(parser("You are the winner", "marcell"), 1)
+	
+	def test_loose_cases(self):
+		self.assertEqual(parser("You are a looser", "marcell"), -1)
+		self.assertEqual(parser("You lost", "marcell"), -1)
+		self.assertEqual(parser("You have lost", "marcell"), -1)
+		self.assertEqual(parser("You've lost", "marcell"), -1)
+		self.assertEqual(parser("Marcell lost", "marcell"), -1)
+		self.assertEqual(parser("Marcell is a looser", "marcell"), -1)
+		self.assertEqual(parser("Marcell lost the game", "marcell"), -1)
+		self.assertEqual(parser("Marcell was the worst", "marcell"), -1)
+		self.assertEqual(parser("Marcell died", "marcell"), -1)
+		self.assertEqual(parser("Marcell is dead", "marcell"), -1)
+		self.assertEqual(parser("You are dead", "marcell"), -1)
+		self.assertEqual(parser("You have no more lifes", "marcell"), -1)
+		self.assertEqual(parser("0 lifes left", "marcell"), -1)
+
+
 
 
 		pass
+	
+	def test_parser_exceptions(self):
+		with self.assertRaises(ValueError):
+			parser("asdasd", "")
+
+		with self.assertRaises(ValueError):
+			parser("", "asdasd")
 
 
 
