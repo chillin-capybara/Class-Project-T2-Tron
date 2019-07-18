49553d2ce87ebf25b3c8ba63f98d77c8a838cf12
Marcell Pigniczki <marceip97@gmail.com>
2019-07-19 00:38:14 +0200

    Asciii mode TRON

---
+++ curs.py
@@ -0,0 +1,25 @@
+import curses
+import os
+
+def main(win):
+	win.nodelay(True)
+	key=""
+	win.clear()                
+	win.addstr("Detected key:")
+	while 1:          
+		try:                 
+			key = win.getkey()         
+			win.clear()                
+			win.addstr("Detected key:")
+			win.addstr(str(key))
+			if key == os.linesep:
+				break           
+		except Exception as e:
+			# No input   
+			pass         
+
+
+while True:
+	ins = input(">>>")
+	if ins == "fuck":
+		curses.wrapper(main)
